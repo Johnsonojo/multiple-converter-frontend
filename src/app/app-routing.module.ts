d27094024caf687e5bb710fc84b0aaad0bae5de9
Johnsonojo <johnsonojo89@gmail.com>
2021-02-04 12:48:59 +0100

    add home module files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'temperature',
+    loadChildren: () =>
+      import('./temperature/temperature.module').then(
+        (m) => m.TemperatureModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
